f2bab875f7017775ff719aee4e18f8f8f8e7da95
Jayendra Prakash <jayendraprakash@Jayendras-MacBook-Air.local>
2025-05-03 19:47:40 +0530

    header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Explore a wide variety of cuisines from our diverse menu and enjoy your favorite dishes delivered straight to your doorstep, hot, fresh, and full of flavor. </p>
+            <button>View Menu</button>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
